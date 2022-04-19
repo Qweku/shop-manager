4ecde464f4a72c927ffacd520f87ed4db64eceb6
Qweku <brobbeycollins96@gmail.com>
2022-04-19 07:06:22 +0000

    product view re-design

--- lib/pages/productView.dart
+++ lib/pages/productView.dart
@@ -2,11 +2,12 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shop_manager/components/button.dart';
 import 'package:shop_manager/components/textFields.dart';
 import 'package:shop_manager/models/GeneralProvider.dart';
 import 'package:shop_manager/models/ShopModel.dart';
 import 'package:shop_manager/pages/productCalculator.dart';
-
+import 'package:shop_manager/pages/widgets/clipPath.dart';
 
 class ProductView extends StatelessWidget {
   final Product product;
@@ -34,223 +35,161 @@ class ProductView extends StatelessWidget {
                 )),
           )
         ],
-        backgroundColor: Colors.transparent,
+        backgroundColor: theme.primaryColor,
         elevation: 0,
       ),
-      backgroundColor: theme.primaryColor,
+      //backgroundColor: theme.primaryColor,
       resizeToAvoidBottomInset: true,
       body: SafeArea(
-          child: Padding(
-        padding: EdgeInsets.all(height * 0.05),
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              SizedBox(height: height * 0.1),
-              Stack(
-                clipBehavior: Clip.none,
-                children: [
-                  Container(
-                    padding: EdgeInsets.all(height * 0.03),
+          child: SingleChildScrollView(
+        child: Stack(
+          children: [
+            Column(
+              children: [
+                ClipPath(
+                  clipper: BottomLineClipper(),
+                  child: Container(
                     width: width,
-                    height: height * 0.4,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(30),
-                      color: theme.primaryColorLight,
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        SizedBox(height: height * 0.15),
-                        Text(
-                          product.productName!,
-                          style: theme.textTheme.headline1!.copyWith(
-                              fontSize: 30, color: theme.primaryColor),
-                        ),
-                        SizedBox(height: height * 0.02),
-                        Text('GHS ${product.sellingPrice}',
-                            style: theme.textTheme.headline1),
-                      ],
-                    ),
+                    color: theme.primaryColor,
+                    height: height * 0.2,
                   ),
-                  Positioned(
-                    top: -height * 0.1,
-                    left: width * 0.17,
-                    //right: 0,
-                    //bottom: 0,
-                    child: Container(
-                        width: width * 0.45,
-                        height: height * 0.25,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(100),
-                          color: (product.imageb64 ?? "").isEmpty
-                              ? Colors.amber
-                              : Colors.transparent,
-                        ),
-                        child: (product.imageb64 ?? "").isEmpty
-                            ? Center(
-                                child: Text(
-                                  product.productName!
-                                      .substring(0, 2)
-                                      .toUpperCase(),
-                                  style: theme.textTheme.headline1!.copyWith(
-                                      fontSize: 70, color: Colors.white),
-                                ),
-                              )
-                            : ClipRRect(
-                                borderRadius: BorderRadius.circular(100),
-                                child: Container(
-                                  width: width * 0.45,
-                                  height: height * 0.25,
-                                  decoration: BoxDecoration(
-                                      image: DecorationImage(
-                                          image: MemoryImage(
-                                              base64Decode(product.imageb64!)),
-                                          fit: BoxFit.cover)),
-                                ),
-                              )
-                        // CircleAvatar(
-                        //     foregroundImage:
-                        //         MemoryImage(base64Decode(product.imageb64!))
-                        //     // Image.memory(
-                        //     //     base64Decode(product.imageb64!),
-                        //     //     fit: BoxFit.fill,
-                        //     //   ),
-                        //     ),
-                        ),
+                ),
+                SizedBox(height: height * 0.15),
+                Text(
+                  product.productName!,
+                  style: theme.textTheme.headline1!
+                      .copyWith(fontSize: 30, color: theme.primaryColor),
+                ),
+                SizedBox(height: height * 0.02),
+                Text('GHS ${product.sellingPrice}',
+                    style: theme.textTheme.headline1),
+                Padding(
+                  padding: EdgeInsets.all(height * 0.02),
+                  child: Column(
+                    //crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Max Quantity',
+                          style: theme.textTheme.bodyText1!
+                              .copyWith(fontSize: 12)),
+                      SizedBox(height: height * 0.01),
+                      Text("${product.quantity!}",
+                          style: theme.textTheme.headline1!
+                              .copyWith(color: theme.primaryColor)),
+                    ],
                   ),
-                  Positioned(
-                      bottom: 0,
-                      right: 0,
-                      left: width * 0.25,
-                      child: Padding(
-                        padding: EdgeInsets.all(height * 0.02),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Column(
-                              //crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text('Max Quantity',
-                                    style: theme.textTheme.bodyText1!
-                                        .copyWith(fontSize: 12)),
-                                SizedBox(height: height * 0.005),
-                                Text("${product.quantity!}",
-                                    style: theme.textTheme.headline1!
-                                        .copyWith(color: theme.primaryColor)),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ))
-                ],
-              ),
-              SizedBox(
-                height: height * 0.02,
-              ),
-              SizedBox(
-                width: width,
-                height: height * 0.1,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Expanded(
-                      flex: 1,
-                      child: Container(
-                        padding: EdgeInsets.all(height * 0.02),
+                ),
+                SizedBox(
+                  height: height * 0.02,
+                ),
+                SizedBox(
+                  width: width * 0.7,
+                  height: height * 0.1,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        padding: EdgeInsets.all(height * 0.01),
                         decoration: BoxDecoration(
                             borderRadius: BorderRadius.circular(10),
-                            color: theme.primaryColorLight),
+                            color: theme.primaryColor),
                         child: GestureDetector(
                             onTap: () {},
                             child: Icon(Icons.remove,
-                                size: 25, color: theme.primaryColor)),
+                                size: 20, color: theme.primaryColorLight)),
                       ),
-                    ),
-                    SizedBox(
-                      width: width * 0.01,
-                    ),
-                    Expanded(
-                      flex: 3,
-                      child: SizedBox(
-                        width: width,
-                        // height: height * 0.1,
-                        child: CustomTextField(
-                          borderColor: theme.primaryColorLight,
-                          textAlign: TextAlign.center,
-                          keyboard: TextInputType.number,
+                      // SizedBox(
+                      //   width: width * 0.01,
+                      // ),
+                      Expanded(
+                        flex: 3,
+                        child: SizedBox(
+                          width: width,
+                          // height: height * 0.1,
+                          child: CustomTextField(
+                            //borderColor: theme.primaryColorLight,
+                            textAlign: TextAlign.center,
+                            keyboard: TextInputType.number,
 
-                          hintColor: theme.primaryColorLight,
-                          style: theme.textTheme.bodyText2!
-                              .copyWith(color: theme.primaryColor),
-                          hintText: '1',
-                          //borderColor: theme.primaryColorLight
+                            hintColor: theme.primaryColor,
+                            style: theme.textTheme.headline2!
+                                .copyWith(color: theme.primaryColor),
+                            hintText: '1',
+                            //borderColor: theme.primaryColorLight
+                          ),
                         ),
                       ),
-                    ),
-                    SizedBox(
-                      width: width * 0.01,
-                    ),
-                    Expanded(
-                      flex: 1,
-                      child: Container(
-                        padding: EdgeInsets.all(height * 0.02),
+                      // SizedBox(
+                      //   width: width * 0.01,
+                      // ),
+                      Container(
+                        padding: EdgeInsets.all(height * 0.01),
                         decoration: BoxDecoration(
                             borderRadius: BorderRadius.circular(10),
-                            color: theme.primaryColorLight),
+                            color: theme.primaryColor),
                         child: GestureDetector(
                             onTap: () {},
                             child: Icon(Icons.add,
-                                size: 25, color: theme.primaryColor)),
+                                size: 20, color: theme.primaryColorLight)),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-              SizedBox(
-                height: height * 0.05,
-              ),
-              GestureDetector(
-                onTap: (() {
-                  if (!(Provider.of<GeneralProvider>(context, listen: false)
-                      .cart
-                      .contains(product))) {
-                    Provider.of<GeneralProvider>(context, listen: false)
-                        .addtoCart(product);
-                  }
-                }),
-                child: Container(
-                  padding: EdgeInsets.symmetric(vertical: height * 0.01),
-                  // width: width,
-                  // height: height * 0.1,
+                SizedBox(
+                  height: height * 0.05,
+                ),
+                Button(
+                  onTap: (() {
+                    if (!(Provider.of<GeneralProvider>(context, listen: false)
+                        .cart
+                        .contains(product))) {
+                      Provider.of<GeneralProvider>(context, listen: false)
+                          .addtoCart(product);
+                    }
+                  }),
+                  buttonText: 'Add to Cart',
+                  color:
+                      (context.watch<GeneralProvider>().cart.contains(product))
+                          ? Colors.blueGrey
+                          : theme.primaryColor,
+                  width: width * 0.7,
+                )
+              ],
+            ),
+            Positioned(
+              top: height * 0.05,
+              left: width * 0.27,
+              //right: 0,
+              //bottom: 0,
+              child: Container(
+                  width: width * 0.45,
+                  height: height * 0.25,
                   decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(30),
-                    color: (context
-                            .watch<GeneralProvider>()
-                            .cart
-                            .contains(product))
-                        ? Colors.blueGrey
-                        : theme.primaryColorLight,
-                  ),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(30),
-                    child: Container(
-                      width: width * 0.5,
-                      padding: EdgeInsets.all(height * 0.01),
-                      color: Colors.transparent,
-                      child: Text(
-                        'Add to Cart',
-                        textAlign: TextAlign.center,
-                        style: theme.textTheme.bodyText2!.copyWith(
-                            color: theme.primaryColor,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 17),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
+                      borderRadius: BorderRadius.circular(height * 0.1),
+                      color: (product.imageb64 ?? "").isEmpty
+                          ? theme.primaryColor
+                          : theme.primaryColorLight,
+                      border:
+                          Border.all(color: theme.primaryColorLight, width: 3)),
+                  child: (product.imageb64 ?? "").isEmpty
+                      ? Center(
+                          child: Text(
+                            product.productName!.substring(0, 2).toUpperCase(),
+                            style: theme.textTheme.headline1!
+                                .copyWith(fontSize: 70, color: Colors.white),
+                          ),
+                        )
+                      : Container(
+                          width: width * 0.45,
+                          height: height * 0.25,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(height * 0.1),
+                              image: DecorationImage(
+                                  image: MemoryImage(
+                                      base64Decode(product.imageb64!)),
+                                  fit: BoxFit.cover)),
+                        )),
+            ),
+          ],
         ),
       )),
     );
