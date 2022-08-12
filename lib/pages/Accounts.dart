493ad9c2deaf3bcb8ed852efc95644dc12f79135
Qweku <brobbeycollins96@gmail.com>
2022-08-12 10:00:23 +0000

    account search

--- lib/pages/Accounts.dart
+++ lib/pages/Accounts.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:shop_manager/components/button.dart';
+import 'package:shop_manager/components/textFields.dart';
 import 'package:shop_manager/pages/widgets/clipPath.dart';
 
 class Accounts extends StatefulWidget {
@@ -10,6 +12,8 @@ class Accounts extends StatefulWidget {
 }
 
 class _AccountsState extends State<Accounts> {
+  final fromDate = TextEditingController();
+  final toDate = TextEditingController();
   @override
   Widget build(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
@@ -26,7 +30,7 @@ class _AccountsState extends State<Accounts> {
                 padding: EdgeInsets.only(
                     right: height * 0.02,
                     left: height * 0.02,
-                    top: widget.tag=='dashboard'?0:height * 0.13,
+                    top: widget.tag == 'dashboard' ? 0 : height * 0.13,
                     bottom: height * 0.07),
                 color: theme.primaryColor,
                 child: HeaderSection(
@@ -34,32 +38,39 @@ class _AccountsState extends State<Accounts> {
                   height: height,
                   width: width,
                   theme: theme,
-                  onPressed: () {},
+                  trailing: IconButton(
+                    icon: const Icon(Icons.search, size: 40, color: Colors.white),
+                    onPressed: () {
+                      _searchAccount(context);
+                    },
+                  ),
                 ),
               ),
             ),
-            
             Padding(
               padding: EdgeInsets.symmetric(
                 horizontal: width * 0.05,
               ),
               child: Container(
-                width: width*0.9,
-                padding: EdgeInsets.all(width*0.05),
+                width: width * 0.9,
+                padding: EdgeInsets.all(width * 0.05),
                 decoration: BoxDecoration(
-                  color:Color.fromARGB(255, 239, 245, 255),
-                  borderRadius: BorderRadius.circular(20)
-                ),
+                    color: const Color.fromARGB(255, 239, 245, 255),
+                    borderRadius: BorderRadius.circular(20)),
                 child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                  Text('Grand Total',style:theme.textTheme.headline1!.copyWith(fontSize:17)),
-                  SizedBox(height:height*0.01),
-                   Text('GHS2000.00',style:theme.textTheme.headline1!.copyWith(color:theme.primaryColor)),
-                ]),
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('Grand Total',
+                          style: theme.textTheme.headline1!
+                              .copyWith(fontSize: 17)),
+                      SizedBox(height: height * 0.01),
+                      Text('GHS2000.00',
+                          style: theme.textTheme.headline1!
+                              .copyWith(color: theme.primaryColor)),
+                    ]),
               ),
             ),
-            SizedBox(height:height*0.02),
+            SizedBox(height: height * 0.02),
             Padding(
               padding: EdgeInsets.symmetric(
                 horizontal: width * 0.05,
@@ -68,18 +79,20 @@ class _AccountsState extends State<Accounts> {
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   SizedBox(
-                    width:width*0.3,
+                      width: width * 0.3,
                       child: Text('Item Name',
                           style: theme.textTheme.bodyText1!
                               .copyWith(fontWeight: FontWeight.bold))),
                   SizedBox(
-                    width:width*0.3,
-                      child: Text('Quantity',textAlign: TextAlign.center,
+                      width: width * 0.3,
+                      child: Text('Quantity',
+                          textAlign: TextAlign.center,
                           style: theme.textTheme.bodyText1!
                               .copyWith(fontWeight: FontWeight.bold))),
                   SizedBox(
-                    width:width*0.3,
-                      child: Text('Total',textAlign: TextAlign.end,
+                      width: width * 0.3,
+                      child: Text('Total',
+                          textAlign: TextAlign.end,
                           style: theme.textTheme.bodyText1!
                               .copyWith(fontWeight: FontWeight.bold))),
                 ],
@@ -95,7 +108,7 @@ class _AccountsState extends State<Accounts> {
                     return Padding(
                       padding: EdgeInsets.symmetric(
                           horizontal: width * 0.05, vertical: height * 0.01),
-                      child: AccountList(width:width,theme: theme),
+                      child: AccountList(width: width, theme: theme),
                     );
                   }),
             )
@@ -104,12 +117,83 @@ class _AccountsState extends State<Accounts> {
       ),
     );
   }
+
+  void _searchAccount(context) {
+    final theme = Theme.of(context);
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+    showModalBottomSheet(
+        isScrollControlled: true,
+        backgroundColor: theme.primaryColor,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(20.0),
+              topRight: Radius.circular(20.0)),
+        ),
+        context: context,
+        builder: (BuildContext bc) {
+          return Container(
+            height: height * 0.7,
+            padding: EdgeInsets.all(height * 0.02),
+            child: Wrap(
+              spacing: 20,
+              children: <Widget>[
+                SizedBox(height: height * 0.02),
+                Padding(
+                  padding: EdgeInsets.only(
+                      bottom: height * 0.02, top: height * 0.04),
+                  child: Text("Search Account History",
+                      style: theme.textTheme.headline2),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(
+                    bottom: height * 0.04,
+                  ),
+                  child: DateTextField(
+                      controller: fromDate,
+                      hintText: 'From',
+                      hintColor: Colors.white,
+                      borderColor: Colors.white,
+                      prefixIcon: const Icon(Icons.calendar_today,
+                          color: Colors.white, size: 20),
+                      style: theme.textTheme.bodyText2),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(
+                    bottom: height * 0.04,
+                  ),
+                  child: DateTextField(
+                      controller: toDate,
+                      hintText: 'To',
+                      hintColor: Colors.white,
+                      borderColor: Colors.white,
+                      prefixIcon: const Icon(Icons.calendar_today,
+                          color: Colors.white, size: 20),
+                      style: theme.textTheme.bodyText2),
+                ),
+                SizedBox(height: height * 0.1),
+                Button(
+                  color: theme.primaryColorLight,
+                  textColor: theme.primaryColor,
+                  width: width,
+                  buttonText: "Done",
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                SizedBox(height: height * 0.4),
+              ],
+            ),
+          );
+        });
+  }
 }
 
 class AccountList extends StatelessWidget {
   const AccountList({
     Key? key,
-    required this.theme, required this.width,
+    required this.theme,
+    required this.width,
   }) : super(key: key);
 
   final ThemeData theme;
@@ -120,18 +204,17 @@ class AccountList extends StatelessWidget {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-         SizedBox(
-                    width:width*0.3,
-                      child: Text('Milk',
-                          style: theme.textTheme.bodyText1)),
-                  SizedBox(
-                    width:width*0.3,
-                      child: Text('2',textAlign: TextAlign.center,
-                          style: theme.textTheme.bodyText1)),
-                  SizedBox(
-                    width:width*0.3,
-                      child: Text('GHS12.00',textAlign: TextAlign.end,
-                          style: theme.textTheme.bodyText1)),
+        SizedBox(
+            width: width * 0.3,
+            child: Text('Milk', style: theme.textTheme.bodyText1)),
+        SizedBox(
+            width: width * 0.3,
+            child: Text('2',
+                textAlign: TextAlign.center, style: theme.textTheme.bodyText1)),
+        SizedBox(
+            width: width * 0.3,
+            child: Text('GHS12.00',
+                textAlign: TextAlign.end, style: theme.textTheme.bodyText1)),
       ],
     );
   }
